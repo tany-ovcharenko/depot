f0bf8a5e152b8149228457e45f22a34c79a2a36a
tany-ovcharenko <dhost71@gmail.com>
2014-12-11 23:49:23 +0300

    7 validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :price, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to:0.01}
+  validates :title, :uniqueness => {
+    message: "UNIQUE!!!"
+  }
+  validates :image_url, allow_blank: true, format: {
+      with: %r{\.(gif|jpg|png)}i,#/\.(gif|jpg|png)/i, 
+      message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
+  }
+  validates :title, :length => {:minimum => 10 }
 end
