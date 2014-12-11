a4464023b1ebc05640af6cc724e627d09d956170
tany-ovcharenko <dhost71@gmail.com>
2014-12-12 01:32:18 +0300

    9 cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,15 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  # убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар
+  def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+          return true
+      else
+          errors.add(:base, 'существуют товарные позиции')
+          return false
+      end
+  end
   validates :title, :description, :price, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to:0.01}
   validates :title, :uniqueness => {
